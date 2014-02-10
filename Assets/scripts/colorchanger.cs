e94f8fb35a78bec61255e82b96b3a1ec19b7ae18
Ben Walthall <bww209@nyu.edu>
2014-02-10 11:38:03 -0500

    first commit

---
+++ Assets/scripts/colorchanger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorchanger : MonoBehaviour {
+	void OnCollisionEnter (Collider info)
+	{
+		if(info.tag == "Player") renderer.material.color = Color.blue;
+	}
+}
